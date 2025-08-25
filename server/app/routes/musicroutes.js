706df2ce87c798c6291740db69181868ea1f57d8
CommitToCode <rishi2020.rp@gmail.com>
2025-08-25 12:17:29 +0530

    login

--- server/app/routes/musicroutes.js
+++ server/app/routes/musicroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Track = require('../models/track');   // ✅ use Track model
+const Track = require('../models/songupload'); 
 const { ensureAuth } = require('../middleware/authsong');       
 const subscriptionCheck = require('../middleware/subscriptioncheck');  
 const { uploadSong } = require('../controllers/songuploadcontroller'); 
